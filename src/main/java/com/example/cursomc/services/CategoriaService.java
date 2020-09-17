ea38a5808162cc1bb4f04078cdacf85afcfb955d
marcowinkler <marcowinkler10@gmail.com>
2020-09-16 22:06:41 -0300

    criando operacao de instanciacao

---
+++ src/main/java/com/example/cursomc/services/CategoriaService.java
@@ -0,0 +1,22 @@
+package com.example.cursomc.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.repositories.CategoriaRepository;
+
+@Service
+public class CategoriaService {
+
+	@Autowired
+	private CategoriaRepository repo;
+
+	public Categoria buscar(Integer id) {
+
+		Categoria cat = repo.findById(id).get();
+
+		return cat;
+	}
+
+}

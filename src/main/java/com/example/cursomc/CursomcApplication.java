5111e98b977074b1eb6a58965eb63f6daf1d668c
marcowinkler <marcowinkler10@gmail.com>
2020-09-17 23:14:15 -0300

    Estado e Cidade

--- src/main/java/com/example/cursomc/CursomcApplication.java
+++ src/main/java/com/example/cursomc/CursomcApplication.java
@@ -8,8 +8,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.example.cursomc.domain.Categoria;
+import com.example.cursomc.domain.Cidade;
+import com.example.cursomc.domain.Estado;
 import com.example.cursomc.domain.Produto;
 import com.example.cursomc.repositories.CategoriaRepository;
+import com.example.cursomc.repositories.CidadeRepository;
+import com.example.cursomc.repositories.EstadoRepository;
 import com.example.cursomc.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -19,8 +23,13 @@ public class CursomcApplication implements CommandLineRunner {
 	private CategoriaRepository categoriaRepository;
 	@Autowired
 	private ProdutoRepository produtoRepository;
-	
-	
+
+	@Autowired
+	private CidadeRepository cidadeRepository;
+
+	@Autowired
+	private EstadoRepository estadoRepository;
+
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
@@ -31,23 +40,33 @@ public class CursomcApplication implements CommandLineRunner {
 
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
-		
-		
-		 Produto prod1 = new Produto(null,"Computador",2000.00); Produto prod2 = new
-		 Produto(null,"Impressora",800.00); Produto prod3 = new
-		 Produto(null,"Mouse",80.00);
 
-	
+		Produto prod1 = new Produto(null, "Computador", 2000.00);
+		Produto prod2 = new Produto(null, "Impressora", 800.00);
+		Produto prod3 = new Produto(null, "Mouse", 80.00);
+
 		cat1.getProdutos().addAll(Arrays.asList(prod1, prod2, prod3));
-		cat2.getProdutos().addAll(Arrays.asList(prod2));		
-		
+		cat2.getProdutos().addAll(Arrays.asList(prod2));
+
 		prod1.getCategorias().addAll(Arrays.asList(cat1));
-		prod2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		prod2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		prod3.getCategorias().addAll(Arrays.asList(cat1));
-						
+
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-		produtoRepository.saveAll(Arrays.asList(prod1,prod2,prod3));
-		
+		produtoRepository.saveAll(Arrays.asList(prod1, prod2, prod3));
+
+		Estado est1 = new Estado(null, "Parana");
+		Estado est2 = new Estado(null, "Santa Catarina");
+
+		Cidade c1 = new Cidade(null, "Curitiba", est1);
+		Cidade c2 = new Cidade(null, "TamandaYork", est1);
+		Cidade c3 = new Cidade(null, "Porto União", est2);
+
+		est1.getCidades().addAll(Arrays.asList(c1, c2));
+		est2.getCidades().addAll(Arrays.asList(c3));
+
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(c1, c2, c3));
 
 	}
 

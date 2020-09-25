6530c8ae947b77fb689b08382637bb5a4e16dc2e
marcowinkler <marcowinkler10@gmail.com>
2020-09-24 22:46:59 -0300

    Categoria DTO

--- src/main/java/com/example/cursomc/services/CategoriaService.java
+++ src/main/java/com/example/cursomc/services/CategoriaService.java
@@ -1,5 +1,6 @@
 package com.example.cursomc.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -45,6 +46,11 @@ public class CategoriaService {
 		} catch (DataIntegrityViolationException e) {
 			throw new DataIntegrityException("Não é possivel excluir uma categoria que não possui produtos");
 		}
+	}
+
+	public List<Categoria> findAll() {
+
+		return repo.findAll();
 	}
 
 }
